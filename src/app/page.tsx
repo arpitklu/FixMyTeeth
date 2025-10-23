e04792b7dfdd1b795b84b9ee44c7de630f7e07a1
arpitklu <arrpiit2004@gmail.com>
2025-10-23 23:36:28 +0530

    feat: Codebase and Authentication Setup Done.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignUpButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button>Click Me</Button>
+    <div>
+      <h1>Home Page</h1>
+      <SignedOut>
+        <SignUpButton mode="modal">Sign Up</SignUpButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignUpButton>Logout</SignUpButton>
+      </SignedIn>
+    </div>
   );
 }
